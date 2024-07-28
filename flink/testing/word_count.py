ee3d5886249699a68d73ddee84561989f25ff030
carterchene <carter.chene26@gmail.com>
2024-07-28 15:33:14 -0600

    consuming eventsim w flink locally

--- flink/testing/word_count.py
+++ flink/testing/word_count.py
@@ -1,37 +1,73 @@
+from pyflink.common.typeinfo import Types
 from pyflink.datastream import StreamExecutionEnvironment
-from pyflink.table import StreamTableEnvironment, DataTypes
-from pyflink.table.descriptors import Schema, FileSystem, OldCsv
+from pyflink.datastream.connectors.kafka import FlinkKafkaConsumer
+from pyflink.datastream.formats.json import JsonRowDeserializationSchema
+from pyflink.table import StreamTableEnvironment
 
-def main():
-    env = StreamExecutionEnvironment.get_execution_environment()
-    t_env = StreamTableEnvironment.create(env)
+s_env = StreamExecutionEnvironment.get_execution_environment()
+s_env.add_jars("file:///C:/Users/Carter%20Dakota/portfolio/downloads/flink-sql-connector-kafka-3.2.0-1.19.jar")
+# start a checkpoint every 10,000 ms (10 s)
+s_env.enable_checkpointing(10 * 1000)
+# make sure 5000 ms (5 s) of progress happen between checkpoints
+s_env.get_checkpoint_config().set_min_pause_between_checkpoints(
+    5 * 1000
+)
+# checkpoints have to complete within 5 minute, or are discarded
+s_env.get_checkpoint_config().set_checkpoint_timeout(
+    5 * 1000
+)
+execution_config = s_env.get_config()
+# execution_config.set_parallelism('1')
 
-    # Define the source schema
-    t_env.connect(FileSystem().path('s3://streaming-demo-project/input.txt')) \
-        .with_format(OldCsv()
-                     .field('word', DataTypes.STRING())) \
-        .with_schema(Schema()
-                     .field('word', DataTypes.STRING())) \
-        .create_temporary_table('source')
+t_env = StreamTableEnvironment.create(s_env)
 
-    # Define the sink schema
-    t_env.connect(FileSystem().path('s3://streaming-demo-project/output')) \
-        .with_format(OldCsv()
-                     .field_delimiter('\t')
-                     .field('word', DataTypes.STRING())
-                     .field('count', DataTypes.BIGINT())) \
-        .with_schema(Schema()
-                     .field('word', DataTypes.STRING())
-                     .field('count', DataTypes.BIGINT())) \
-        .create_temporary_table('sink')
+# deserialization_schema = JsonRowDeserializationSchema.builder() \
+#     .type_info(type_info=Types.ROW([Types.INT(), Types.STRING()])).build()
 
-    # Run the word count
-    t_env.from_path('source') \
-        .group_by('word') \
-        .select('word, COUNT(1) as count') \
-        .insert_into('sink')
+# can test the ec2 instance kafka server from here using the external ip it
+# kafka_consumer = FlinkKafkaConsumer(
+#     topics='listen_events',
+#     deserialization_schema=deserialization_schema,
+#     properties={'bootstrap.servers': '3.98.123.24:9092'})
+# 
+# ds = env.add_source(kafka_consumer)
 
-    t_env.execute('word count')
+t_env.execute_sql("""
+    CREATE TABLE kafka_source (
+        event_data STRING
+    ) WITH (
+        'connector' = 'kafka',
+        'topic' = 'listen_events',
+        'properties.bootstrap.servers' = 'EXTERNAL_ADDRESS_HERE:9092',
+        'properties.group.id' = 'flink-consumer-group-1', 
+        'scan.startup.mode' = 'earliest-offset',
+        'format' = 'raw'
+    )
+""")
 
-if __name__ == 'main':
-    main()
+t_env.execute_sql("""
+    CREATE TABLE print_sink (
+        event_data STRING
+    ) WITH (
+        'connector' = 'print'
+    )
+""")
+print('straeting flink job to transfer from kafka topic to print sink')
+t_env.execute_sql("INSERT INTO print_sink SELECT * FROM kafka_source").wait()
+
+# t_env.execute_sql("""
+#     CREATE TABLE csv_sink (
+#         event_data STRING
+#     ) WITH (
+#         'connector' = 'filesystem',
+#         'path' = 'file:///C:/Users/Carter%20Dakota/portfolio/downloads/output.csv',
+#         'format' = 'csv'
+#     )
+# """)
+
+# t_env.execute_sql("INSERT INTO csv_sink SELECT * FROM kafka_source").wait()
+
+
+#with ds.execute_and_collect() as results:
+#    for result in results:
+#        print(result)
