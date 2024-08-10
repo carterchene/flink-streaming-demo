e1d112ea30f25c2635ecca5d8fcbc2bec1a85630
carterchene <carter.chene26@gmail.com>
2024-08-10 13:28:16 -0600

    flink job for all kafka topics

--- flink/stream_all_events.py
+++ flink/stream_all_events.py
@@ -1,100 +1,367 @@
 from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.table import StreamTableEnvironment
 from pyflink.table.expressions import col, lit
+import os
 
-def write_to_s3(t_env):
+KAFKA_BROKER_IP = os.getenv('KAFKA_BROKER_IP')
+S3_BUCKET = os.getenv('S3_BUCKET')
+
+def construct_insert_statement(source, sink) -> str:
+    return f"""
+            INSERT INTO {sink} 
+                SELECT 
+                *, 
+                DATE_FORMAT(TO_TIMESTAMP(FROM_UNIXTIME(`ts`/1000)), 'yyyy') as `year`,
+                DATE_FORMAT(TO_TIMESTAMP(FROM_UNIXTIME(`ts`/1000)), 'MM') as `month`,
+                DATE_FORMAT(TO_TIMESTAMP(FROM_UNIXTIME(`ts`/1000)), 'dd') as `day`
+            FROM {source}
+            """
+
+
+def stream_listen_events(t_env):
     
-    t_env.execute_sql("""
-        CREATE TABLE kafka_source (
-            `song` string ,
-            `artist` string ,
-            `duration` double ,
-            `ts` bigint ,
-            `sessionId` INTEGER ,
-            `auth` string ,
-            `level` string ,
-            `itemInSession` INTEGER ,
-            `city` string ,
-            `zip` string ,
-            `state` string ,
-            `userAgent` string ,
-            `lon` double ,
-            `lat` double ,
-            `userId` integer ,
-            `lastName` string ,
-            `firstName` string ,
-            `gender` string ,
+    sink_table = 'listen_events'
+    source_table = 'kafka_listen_events'
+
+    t_env.execute_sql(f"""
+        CREATE TABLE {source_table} (
+            `song` string,
+            `artist` string,
+            `duration` double,
+            `ts` bigint,
+            `sessionId` INTEGER,
+            `auth` string,
+            `level` string,
+            `itemInSession` INTEGER,
+            `city` string,
+            `zip` string,
+            `state` string,
+            `userAgent` string,
+            `lon` double,
+            `lat` double,
+            `userId` integer,
+            `lastName` string,
+            `firstName` string,
+            `gender` string,
             `registration` BIGINT
         ) WITH (
             'connector' = 'kafka',
             'topic' = 'listen_events',
-            'properties.bootstrap.servers' = '35.183.28.240:9092',
-            'properties.group.id' = 'flink-consumer-group-1', 
+            'properties.bootstrap.servers' = '{KAFKA_BROKER_IP}:9092',
+            'properties.group.id' = 'flink-consumer-group-1',
             'scan.startup.mode' = 'latest-offset',
             'format' = 'json'
         )
     """)
 
-    t_env.execute_sql("""
-        CREATE TABLE listen_events_s3 (
-            `song` string ,
-            `artist` string ,
-            `duration` double ,
-            `ts` bigint ,
-            `sessionId` INTEGER ,
-            `auth` string ,
-            `level` string ,
-            `itemInSession` INTEGER ,
-            `city` string ,
-            `zip` string ,
-            `state` string ,
-            `userAgent` string ,
-            `lon` double ,
-            `lat` double ,
-            `userId` integer ,
-            `lastName` string ,
-            `firstName` string ,
-            `gender` string ,
-            `registration` BIGINT
-            ) WITH (
-                   'connector' = 'filesystem',
-                'path' = 's3://streaming-demo-project/outputs',
-                'format' = 'parquet',
-                'sink.rolling-policy.file-size' = '128MB',
-                'sink.rolling-policy.rollover-interval' = '15 min',
-                'sink.rolling-policy.check-interval' = '1 min',
-                'partition.time-extractor.timestamp-pattern'='$year-$month-$day 00:00:00',
-                'sink.partition-commit.delay'='1 h',
-                'sink.partition-commit.policy.kind'='success-file'
-            )
+    t_env.execute_sql(f"""
+        CREATE TABLE {sink_table} (
+            `song` string,
+            `artist` string,
+            `duration` double,
+            `ts` bigint,
+            `sessionId` INTEGER,
+            `auth` string,
+            `level` string,
+            `itemInSession` INTEGER,
+            `city` string,
+            `zip` string,
+            `state` string,
+            `userAgent` string,
+            `lon` double,
+            `lat` double,
+            `userId` integer,
+            `lastName` string,
+            `firstName` string,
+            `gender` string,
+            `registration` BIGINT,
+            `year` STRING,
+            `month` STRING,
+            `day` STRING
+        ) PARTITIONED BY (`year`, `month`, `day`) WITH (
+            'connector' = 'filesystem',
+            'path' = 's3://streaming-demo-project/outputs/{sink_table}',
+            'format' = 'parquet',
+            'sink.rolling-policy.file-size' = '128MB',
+            'sink.rolling-policy.rollover-interval' = '5 min',
+            'sink.rolling-policy.check-interval' = '1 min',
+            'partition.time-extractor.timestamp-pattern'='$year-$month-$day 00:00:00',
+            'sink.partition-commit.delay'='5 min',
+            'sink.partition-commit.policy.kind'='success-file',
+            'sink.partition-commit.trigger' = 'process-time',
+            'sink.partition-commit.watermark-time-zone' = 'UTC'
+        )
     """)
+
     stmt_set = t_env.create_statement_set()
-    stmt_set.add_insert_sql("""INSERT INTO listen_events_s3 SELECT * FROM kafka_source;""")
+    stmt_set.add_insert_sql(construct_insert_statement(source_table, sink_table))
     
     job = stmt_set.execute()
-
-    print(
-          f"""Job status: {job.get_job_client().get_job_status()}"""
-    )
-
-    # t_env.execute('stream to s3')
+    print(f"Job status: {job.get_job_client().get_job_status()}")
 
 
+def stream_auth_events(t_env):
+
+    sink_table = 'auth_events'
+    source_table = 'kafka_auth_events'
+
+    t_env.execute_sql(f"""
+        CREATE TABLE {source_table} (
+            `ts` BIGINT,
+            `sessionId` STRING,
+            `level` STRING,
+            `itemInSession` INT,
+            `city` STRING,
+            `zip` STRING,
+            `state` STRING,
+            `userAgent` STRING,
+            `lon` DOUBLE,
+            `lat` DOUBLE,
+            `userId` STRING,
+            `lastName` STRING,
+            `firstName` STRING,
+            `gender` STRING,
+            `registration` BIGINT,
+            `success` BOOLEAN 
+        ) WITH (
+            'connector' = 'kafka',
+            'topic' = 'auth_events',
+            'properties.bootstrap.servers' = '{KAFKA_BROKER_IP}:9092',
+            'properties.group.id' = 'flink-consumer-group-1',
+            'scan.startup.mode' = 'latest-offset',
+            'format' = 'json'
+        )
+    """)
+
+    t_env.execute_sql(f"""
+        CREATE TABLE {sink_table} (
+            `ts` BIGINT,
+            `sessionId` STRING,
+            `level` STRING,
+            `itemInSession` INT,
+            `city` STRING,
+            `zip` STRING,
+            `state` STRING,
+            `userAgent` STRING,
+            `lon` DOUBLE,
+            `lat` DOUBLE,
+            `userId` STRING,
+            `lastName` STRING,
+            `firstName` STRING,
+            `gender` STRING,
+            `registration` BIGINT,
+            `success` BOOLEAN, 
+            `year` STRING,
+            `month` string, 
+            `day` string
+        ) PARTITIONED BY (`year`, `month`, `day`) WITH (
+            'connector' = 'filesystem',
+            'path' = 's3://streaming-demo-project/outputs/{sink_table}',
+            'format' = 'parquet',
+            'sink.rolling-policy.file-size' = '128MB',
+            'sink.rolling-policy.rollover-interval' = '5 min',
+            'sink.rolling-policy.check-interval' = '1 min',
+            'partition.time-extractor.timestamp-pattern'='$year-$month-$day 00:00:00',
+            'sink.partition-commit.delay'='5 min',
+            'sink.partition-commit.policy.kind'='success-file',
+            'sink.partition-commit.trigger' = 'process-time',
+            'sink.partition-commit.watermark-time-zone' = 'UTC'
+        )
+    """)
+
+    stmt_set = t_env.create_statement_set()
+    stmt_set.add_insert_sql(construct_insert_statement(source_table, sink_table))
+    
+    job = stmt_set.execute()
+    print(f"Job status: {job.get_job_client().get_job_status()}")
+
+def stream_page_view_events(t_env):
+
+    sink_table = 'page_view_events'
+    source_table = f'kafka_{sink_table}'
+
+    t_env.execute_sql(f"""
+        CREATE TABLE {source_table} (
+            `ts` BIGINT,
+            `sessionId` STRING,
+            `page` STRING,
+            `auth` STRING,
+            `method` STRING,
+            `status` INT,
+            `level` STRING,
+            `itemInSession` INT,
+            `city` STRING,
+            `zip` STRING,
+            `state` STRING,
+            `userAgent` STRING,
+            `lon` DOUBLE,
+            `lat` DOUBLE,
+            `userId` STRING,
+            `lastName` STRING,
+            `firstName` STRING,
+            `gender` STRING,
+            `registration` BIGINT,
+            `artist` STRING,
+            `song` STRING,
+            `duration` DOUBLE 
+        ) WITH (
+            'connector' = 'kafka',
+            'topic' = 'page_view_events',
+            'properties.bootstrap.servers' = '{KAFKA_BROKER_IP}:9092',
+            'properties.group.id' = 'flink-consumer-group-1',
+            'scan.startup.mode' = 'latest-offset',
+            'format' = 'json'
+        )
+    """)
+
+    t_env.execute_sql(f"""
+        CREATE TABLE {sink_table} (
+            `ts` BIGINT,
+            `sessionId` STRING,
+            `page` STRING,
+            `auth` STRING,
+            `method` STRING,
+            `status` INT,
+            `level` STRING,
+            `itemInSession` INT,
+            `city` STRING,
+            `zip` STRING,
+            `state` STRING,
+            `userAgent` STRING,
+            `lon` DOUBLE,
+            `lat` DOUBLE,
+            `userId` STRING,
+            `lastName` STRING,
+            `firstName` STRING,
+            `gender` STRING,
+            `registration` BIGINT,
+            `artist` STRING,
+            `song` STRING,
+            `duration` DOUBLE 
+            `year` STRING,
+            `month` string, 
+            `day` string
+        ) PARTITIONED BY (`year`, `month`, `day`) WITH (
+            'connector' = 'filesystem',
+            'path' = 's3://streaming-demo-project/outputs/{sink_table}',
+            'format' = 'parquet',
+            'sink.rolling-policy.file-size' = '128MB',
+            'sink.rolling-policy.rollover-interval' = '5 min',
+            'sink.rolling-policy.check-interval' = '1 min',
+            'partition.time-extractor.timestamp-pattern'='$year-$month-$day 00:00:00',
+            'sink.partition-commit.delay'='5 min',
+            'sink.partition-commit.policy.kind'='success-file',
+            'sink.partition-commit.trigger' = 'process-time',
+            'sink.partition-commit.watermark-time-zone' = 'UTC'
+        )
+    """)
+
+    stmt_set = t_env.create_statement_set()
+    stmt_set.add_insert_sql(construct_insert_statement(source_table, sink_table))
+    
+    job = stmt_set.execute()
+    print(f"Job status: {job.get_job_client().get_job_status()}")
+
+def stream_status_change_events(t_env):
+
+    sink_table = 'status_change_events'
+    source_table = f'kafka_{sink_table}'
+
+    t_env.execute_sql(f"""
+        CREATE TABLE {source_table} (
+            `ts` BIGINT,
+            `sessionId` STRING,
+            `auth` STRING,
+            `level` STRING,
+            `itemInSession` INT,
+            `city` STRING,
+            `zip` STRING,
+            `state` STRING,
+            `userAgent` STRING,
+            `lon` DOUBLE,
+            `lat` DOUBLE,
+            `userId` STRING,
+            `lastName` STRING,
+            `firstName` STRING,
+            `gender` STRING,
+            `registration` BIGINT
+        ) WITH (
+            'connector' = 'kafka',
+            'topic' = 'status_change_events',
+            'properties.bootstrap.servers' = '{KAFKA_BROKER_IP}:9092',
+            'properties.group.id' = 'flink-consumer-group-1',
+            'scan.startup.mode' = 'latest-offset',
+            'format' = 'json'
+        )
+    """)
+
+    t_env.execute_sql(f"""
+        CREATE TABLE {sink_table} (
+            `ts` BIGINT,
+            `sessionId` STRING,
+            `auth` STRING,
+            `level` STRING,
+            `itemInSession` INT,
+            `city` STRING,
+            `zip` STRING,
+            `state` STRING,
+            `userAgent` STRING,
+            `lon` DOUBLE,
+            `lat` DOUBLE,
+            `userId` STRING,
+            `lastName` STRING,
+            `firstName` STRING,
+            `gender` STRING,
+            `registration` BIGINT
+            `year` STRING,
+            `month` string, 
+            `day` string
+        ) PARTITIONED BY (`year`, `month`, `day`) WITH (
+            'connector' = 'filesystem',
+            'path' = 's3://streaming-demo-project/outputs/{sink_table}',
+            'format' = 'parquet',
+            'sink.rolling-policy.file-size' = '128MB',
+            'sink.rolling-policy.rollover-interval' = '5 min',
+            'sink.rolling-policy.check-interval' = '1 min',
+            'partition.time-extractor.timestamp-pattern'='$year-$month-$day 00:00:00',
+            'sink.partition-commit.delay'='5 min',
+            'sink.partition-commit.policy.kind'='success-file',
+            'sink.partition-commit.trigger' = 'process-time',
+            'sink.partition-commit.watermark-time-zone' = 'UTC'
+        )
+    """)
+
+    stmt_set = t_env.create_statement_set()
+    stmt_set.add_insert_sql(construct_insert_statement(source_table, sink_table))
+    
+    job = stmt_set.execute()
+    print(f"Job status: {job.get_job_client().get_job_status()}")
 
 if __name__ == '__main__':
-
-    # set the streaming environment
+    # Set up the streaming environment
     s_env = StreamExecutionEnvironment.get_execution_environment()
-    # add the kafka connector 
-    # s_env.add_jars("file:///C:/Users/Carter%20Dakota/portfolio/downloads/flink-connector-kafka-3.2.0-1.18.jar")
-    # s_env.add_jars("file:///C:/Users/Carter%20Dakota/portfolio/downloads/flink-s3-fs-presto-1.19.1.jar")
-    # s_env.add_jars("file:///C:/Users/Carter%20Dakota/portfolio/downloads/flink-sql-parquet-1.18.1.jar")
+    
+    # Enable checkpointing
+    s_env.enable_checkpointing(60000)  # 60 seconds
+    s_env.get_checkpoint_config().set_min_pause_between_checkpoints(30000)
+    s_env.get_checkpoint_config().set_checkpoint_timeout(900000)
+    
+    # Add the required JAR files
     s_env.add_jars("file:///lib/flink-sql-connector-kafka-3.2.0-1.18.jar")
     s_env.add_jars("file:///lib/flink-sql-parquet-1.18.1.jar")
     s_env.add_jars("file:///lib/flink-parquet-1.18.1.jar")
-
+    
+    # Create the table environment
     t_env = StreamTableEnvironment.create(s_env)
+    
+    print('Streaming listen events...')
+    stream_listen_events(t_env)
+    
+    print('Streaming auth events...')
+    stream_auth_events(t_env)
 
-    print('writing to s3...')
-    write_to_s3(t_env)
-
+    print('Streaming page view events...')
+    stream_page_view_events(t_env)
+    
+    print('Streaming status change events...')
+    stream_status_change_events(t_env)
--- setup/pyflink_on_EMR.md
+++ setup/pyflink_on_EMR.md
@@ -16,7 +16,47 @@ we could also put our venv there i think, then it would be accessible to all clu
 
 I think to access the flink web UI i need to use a SOCKS proxy. This really only works natively on Firefox. 
 
+I think the steps are as follows:
+
+create the yarn session with
+flink-yarn-session
+
+then you have to submit jobs to that specific yarn session created. so get the application id via
+yarn application -list
+
+then do something like: flink run -m yarn-cluster -yid appication_idfromabove -yn 2 job.jar 
 
 
+##########################
+# execute the statement set
+    # remove .wait if submitting to a remote cluster, refer to
+    # https://nightlies.apache.org/flink/flink-docs-stable/docs/dev/python/faq/#wait-for-jobs-to-finish-when-executing-jobs-in-mini-cluster
+    # for more details
+    statement_set.execute().wait()
+
+look here for how to write proper script:
+https://nightlies.apache.org/flink/flink-docs-master/api/python/examples/datastream/connectors.html
+
+for s3 sink, use s3a:// so it knows its using hadoop and not presto
 
 
+Alright i was able to access the flink ui through port forwarding. I ran a flink job via
+flink run -py word_count.py
+
+that gave this:
+Found Web Interface ip-10-0-20-117.ca-central-1.compute.internal:43217
+
+I accessed it by port forwarding like this: 
+ssh -i kafka-cluster.pem -N -L 8080:ip-10-0-20-117.ca-central-1.compute.internal:43217 hadoop@ec2-15-222-245-242.ca-central-1.compute.amazonaws.com
+
+# Set up instructions
+
+1. Fire up the EMR cluster and the kafka EC2. (this will be terraform later)
+2. ssh into kafka ec2. cd to kafka, change the kafka listener ip in docker-compose.yml to appropriate. docker-compose up
+3. cd to scripts, run bash eventsim_setup.sh. Now events are streaming to kafka topics. verify at ip.of.kafka.server:9021 (this is a webui that lets us see whats goin on)
+4. ssh to primary node of flink cluster. download our script: aws s3 cp s3://s3bucketname/scripts/stream_all_events.py .
+4. create flink session: flink-yarn-session -d
+5. note where the jobmanager ui is hosted (it says in the logs of flink-yarn-session). portfoward to see the flink ui (subbing in for appropriate addresses): ssh -i kafka-cluster.pem -N -L 8080:ip-10-0-20-117.ca-central-1.compute.internal:43217 hadoop@ec2-15-222-245-242.ca-central-1.compute.amazonaws.com
+6. set env variables for the pyflink job: export KAFKA_BROKER_IP=ip.addy and export S3_BUCKET=mys3 
+7. once that works, lets run the flink job. run flink run -py stream_all_events.py from primary node
+8. in flink ui, should see 4 jobs begin. parquets should be getting made in s3 bucket. 
